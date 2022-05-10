ce9fd2c8cf27e009d71f20e82cbe197a66b51f1b
Yuliia <84233109+Yuliia-dev@users.noreply.github.com>
2022-05-10 20:36:50 +0300

    init

--- src/components/App/App.styled.js
+++ src/components/App/App.styled.js
@@ -29,3 +29,16 @@ export const TitleContacts = styled.h2`
   letter-spacing: 0.06em;
   color: ${props => props.theme.colors.red};
 `;
+
+// export const TitleContacts = styled.h2`
+//   display: flex;
+//   justify-content: start;
+//   align-items: center;
+//   margin: 0;
+//   font-style: italic;
+//   font-weight: 700;
+//   font-size: 36px;
+//   line-height: 1.62;
+//   letter-spacing: 0.06em;
+//   color: ${props => props.theme.colors.red};
+// `;
